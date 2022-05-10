3caf7f0b3b4a665c9ecca0ab4d0e8ac88bcd16c0
YoonahPark <86581647+YoonahPark@users.noreply.github.com>
2022-05-10 16:22:57 +0900

    Create 11047.cpp

---
+++ 11047.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int N, K, n, cnt=0;
+    vector<int> v;
+    cin>>N>>K;
+
+    for(int i=0; i<N; i++){
+        cin>>n;
+        v.push_back(n);
+    }
+    
+    int i=v.size()-1;
+    
+    while(K!=0){
+        cnt += K/v[i];
+        K = K%v[i];
+        i--;
+        
+    }
+
+    cout<<cnt;
+}
